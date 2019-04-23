69f4d0621d87dd8091198b42970f975cc5e3da2c
tojose <tojose@rmplc.net>
2019-04-23 19:42:33 +0530

    initial

---
+++ eh-send/AzureBlob/azureblob.py
@@ -0,0 +1,27 @@
+from azure.storage.blob import BlockBlobService
+from AzureBlob import config
+
+
+class AzureBlob:
+    def __init__(self):
+        self.config = config.BLOBS['slimageutils']
+        self.account_key = self.config['key']
+        self.container_name = self.config['container_name']
+        self.account_name = self.config['account_name']
+
+        self.retries = int(self.config['retries'])
+        self.retry_in_seconds = int(self.config['retry_in_seconds'])
+        self.blob_service = BlockBlobService(
+            account_name=self.account_name, account_key=self.account_key)
+
+    def get_image_bytes(self, image_file_name):
+        blob = self.blob_service.get_blob_to_bytes(
+            self.container_name, image_file_name).content
+
+        return blob
+
+    def save_image_bytes(self, image_file_name, image_bytes):
+        blob = self.blob_service.create_blob_from_bytes(
+            self.container_name, image_file_name, blob=image_bytes)
+
+        return True
